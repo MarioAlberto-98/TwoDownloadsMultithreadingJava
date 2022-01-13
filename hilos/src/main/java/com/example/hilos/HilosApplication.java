ab6088966ad1d6abffee50e72469ea21e62fec0e
MarioAlberto-98 <77813457+MarioAlberto-98@users.noreply.github.com>
2022-01-13 16:29:30 -0600

    Descargar dos archivos con multihilamiento

---
+++ hilos/src/main/java/com/example/hilos/HilosApplication.java
@@ -0,0 +1,84 @@
+package com.example.hilos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Arrays;
+
+
+public class HilosApplication implements Runnable {
+
+    String[] urlsList;
+
+    public HilosApplication(String[] urlsList) {
+        this.urlsList = urlsList;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+
+            for(String urlString : urlsList) {
+
+                URL url = new URL(urlString);
+                String filename = urlString.substring(urlString.lastIndexOf("/") + 1).trim() + ".iso";
+                BufferedReader reader =  new BufferedReader(new InputStreamReader(url.openStream()));
+                BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+
+                String line;
+                URLConnection urlConnection=url.openConnection();
+                urlConnection.connect();
+                int file_s=urlConnection.getContentLength();
+
+
+
+                while ((line = reader.readLine()) != null) {
+                    writer.write(line);
+                }
+                System.out.println("Page downloaded to " + filename + "||"+file_s);
+
+                writer.close();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String args[])
+    {
+
+        String[] urls1 = new String[]{"https://downloads-global.3cx.com/downloads/debian10iso/debian-amd64-netinst-3cx"};
+        String[] urls2 = new String[]{"https://downloads-global.3cx.com/downloads/debian10iso/debian-amd64-netinst-3cx"};
+
+
+        Thread downloaderOne = new Thread(new HilosApplication(Arrays.copyOfRange(urls1, 0, urls1.length)));
+        Thread downloaderTwo = new Thread(new HilosApplication(Arrays.copyOfRange(urls2, 0, urls2.length)));
+
+        try {
+
+            long startTime = System.currentTimeMillis();
+            downloaderOne.start();
+            downloaderTwo.start();
+
+            downloaderOne.join();
+            downloaderTwo.join();
+            long endTime = System.currentTimeMillis();
+
+            System.out.println("Total time taken: " + (endTime - startTime) / 1000 + "s");
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
